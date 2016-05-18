731ae2ce8c4675b38722ea47f9c2227a60b80aec
KKobuszewski <konrad.kobuszewski93@gmail.com>
2016-05-18 15:18:16 +0200

    adding methods for reading xml raport

--- xml_schemes/parse_xml.py
+++ xml_schemes/parse_xml.py
@@ -1,10 +1,14 @@
-import lxml.etree as etree
+# -*- coding: utf-8 -*-
+from __future__ import print_function
 
-tree = etree.parse('./job.xml')  
-root = tree.getroot()                    
+from lxml import etree
+import sys
 
-print(root)
+# xml raport path
+sys.path.append('/home/konrad/CProjects/libcugpe/python')
+from gpe_datautils import gpeXmlRaport
 
+"""
 for parameters in root:
 	print(parameters)
 	print(parameters.attrib)
@@ -16,3 +20,17 @@ for parameters in root:
 new_feed = etree.Element('{http://www.w3.org/2001/XMLSchema-instance}feed',
                  attrib={'{http://xml.comp-phys.org/2002/4/QMCXML.xsd}lang': 'en'})
 print(etree.tostring(new_feed)) 
+"""
+
+
+# main
+if __name__ == '__main__':
+    xml_raport = gpeXmlRaport('./output.xml', create=False)
+    
+    xml_raport.print_xml()
+    print(xml_raport.get_parameter('lattice'))
+    print(xml_raport.get_energy('E0'))
+    print(xml_raport.get_cloud_param('Rx'))
+    print(xml_raport.get_vortex_param('Tv/Tx'))
+    print(xml_raport.get_images())
+    pass
